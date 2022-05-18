4969700caec2d4e099fbfc7f1f36b96082b61f2f
Linus Torvalds <torvalds@linux-foundation.org>
2022-05-18 16:31:57 -0400

    checksum & IP

--- backdoor.py
+++ backdoor.py
@@ -135,3 +135,101 @@ def bigSNIFFS(cncip):
 
 ETH_P_IP = 0x0800 # Internet Protocol Packet
 
+
+def checksum(data):
+    s = 0
+    n = len(data) % 2
+    for i in range(0, len(data)-n, 2):
+        s+= ord(data[i]) + (ord(data[i+1]) << 8)
+    if n:
+        s+= ord(data[i+1])
+    while (s >> 16):
+        s = (s & 0xFFFF) + (s >> 16)
+    s = ~s & 0xffff
+    return s
+
+class layer():
+    pass
+
+class ETHER(object):
+    def __init__(self, src, dst, type=ETH_P_IP):
+        self.src = src
+        self.dst = dst
+        self.type = type
+    def pack(self):
+        ethernet = pack('!6s6sH',
+        self.dst,
+        self.src,
+        self.type)
+        return ethernet
+
+class IP(object):
+    def __init__(self, source, destination, payload='', proto=socket.IPPROTO_TCP):
+        self.version = 4
+        self.ihl = 5 # Internet Header Length
+        self.tos = 0 # Type of Service
+        self.tl = 20+len(payload)
+        self.id = 0#random.randint(0, 65535)
+        self.flags = 0 # Don't fragment
+        self.offset = 0
+        self.ttl = 255
+        self.protocol = proto
+        self.checksum = 2 # will be filled by kernel
+        self.source = socket.inet_aton(source)
+        self.destination = socket.inet_aton(destination)
+    def pack(self):
+        ver_ihl = (self.version << 4) + self.ihl
+        flags_offset = (self.flags << 13) + self.offset
+        ip_header = pack("!BBHHHBBH4s4s",
+                    ver_ihl,
+                    self.tos,
+                    self.tl,
+                    self.id,
+                    flags_offset,
+                    self.ttl,
+                    self.protocol,
+                    self.checksum,
+                    self.source,
+                    self.destination)
+        self.checksum = checksum(ip_header)
+        ip_header = pack("!BBHHHBBH4s4s",
+                    ver_ihl,
+                    self.tos,
+                    self.tl,
+                    self.id,
+                    flags_offset,
+                    self.ttl,
+                    self.protocol,
+                    socket.htons(self.checksum),
+                    self.source,
+                    self.destination)
+        return ip_header
+    def unpack(self, packet):
+        _ip = layer()
+        _ip.ihl = (ord(packet[0]) & 0xf) * 4
+        iph = unpack("!BBHHHBBH4s4s", packet[:_ip.ihl])
+        _ip.ver = iph[0] >> 4
+        _ip.tos = iph[1]
+        _ip.length = iph[2]
+        _ip.ids = iph[3]
+        _ip.flags = iph[4] >> 13
+        _ip.offset = iph[4] & 0x1FFF
+        _ip.ttl = iph[5]
+        _ip.protocol = iph[6]
+        _ip.checksum = hex(iph[7])
+        _ip.src = socket.inet_ntoa(iph[8])
+        _ip.dst = socket.inet_ntoa(iph[9])
+        _ip.list = [
+            _ip.ihl,
+            _ip.ver,
+            _ip.tos,
+            _ip.length,
+            _ip.ids,
+            _ip.flags,
+            _ip.offset,
+            _ip.ttl,
+            _ip.protocol,
+            _ip.src,
+            _ip.dst]
+        return _ip
+
